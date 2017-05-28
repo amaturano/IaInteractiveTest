8f36141970394cbecb3787c98b16a9cb23c4f5de
Alexis Maturano <lors_256@outlook.com>
2017-05-28 10:12:53 -0500

    Wrong casting element - FIXED

--- app/src/main/java/com/maturano/alexis/alexismaturanotest/MovieDetails.java
+++ app/src/main/java/com/maturano/alexis/alexismaturanotest/MovieDetails.java
@@ -52,14 +52,14 @@ public class MovieDetails extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
     }
 
